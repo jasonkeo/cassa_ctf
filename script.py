58c95f03b1f0eab057a69c68dd1b13e90b4cd33d
Jason Keo <jasonkeo047@outlook.com>
2024-06-18 14:11:08 +0800

    ffe

--- script.py
+++ script.py
@@ -9,7 +9,7 @@ Patrick outputs 1 for every $5 he makes and outputs 0 for every $5 he losses.
 Patricks ouputs are stored in a list inside of money.py (e.g money = [1,1,0,1]) so in this case he has made $10 over all.
 
 
-The flag for this challenge is the amount of money the Patrick has made in digits. e.g ctf{100}, ctf{-50}
+The flag for this challenge is the amount of money the Patrick has made in digits. e.g pecan{100}, pecan{-50}
 """
 
 
